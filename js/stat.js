ffa5d0dd03e31d68938c0b840023c94418938e88
RomikSharov <semen.ok2@gmail.com>
2017-11-19 12:30:23 +0400

    Оптимизировал цикл, вынес переменные за функцию.

--- js/stat.js
+++ js/stat.js
@@ -2,7 +2,7 @@
 
 var findMax = function (mass) {
   var maxItem = mass[0];
-  for (var i = 0; i < mass.length; i++) {
+  for (var i = 1; i < mass.length; i++) {
     if (maxItem < mass[i]) {
       maxItem = mass[i];
     }
@@ -11,33 +11,15 @@ var findMax = function (mass) {
 };
 
 var renderStatistics = function (ctx, names, times) {
-  var messageRectX = 100;
-  var messageRectY = 10;
-  var shadowOffset = 10;
-  var messageWidth = 420;
-  var messageHeight = 270;
-
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(messageRectX + shadowOffset, messageRectY + shadowOffset, messageWidth, messageHeight);
   ctx.fillStyle = 'white';
   ctx.fillRect(messageRectX, messageRectY, messageWidth, messageHeight);
 
-  var messageTitleX = 130;
-  var messageTitleY = 50;
-  var fontHeight = 16;
   ctx.fillStyle = '#00F';
   ctx.font = '' + fontHeight + 'pt PT Mono';
   ctx.fillText('Ура вы победили!', messageTitleX, messageTitleY);
   ctx.fillText('Список результатов:', messageTitleX, messageTitleY + fontHeight);
-
-  var bestTimes = findMax(times);
-  var histogramLength;
-  var maxHistogramLength = 150;
-  var ratioHistogramLength = bestTimes / maxHistogramLength;
-  var histogramWidth = 40;
-  var distanceBetweenHistogram = 50;
-  var histogramRectY = messageTitleY + fontHeight * 3.125;
-  var histogramTransparency = 1;
 
   for (var i = 0; i < names.length; i++) {
     if (names[i] === 'Вы') {
@@ -48,10 +30,27 @@ var renderStatistics = function (ctx, names, times) {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + histogramTransparency + ' )';
     }
 
+    var bestTimes = findMax(times);
+    var ratioHistogramLength = bestTimes / maxHistogramLength;
     histogramLength = Math.round(times[i] / ratioHistogramLength);
     ctx.fillRect(messageTitleX + i * (histogramWidth + distanceBetweenHistogram), histogramRectY + maxHistogramLength - histogramLength, histogramWidth, histogramLength);
-    ctx.font = '16pt PT Mono';
+    ctx.font = '' + fontHeight + 'pt PT Mono';
     ctx.fillText(names[i], messageTitleX + i * (histogramWidth + distanceBetweenHistogram), messageHeight);
     ctx.fillText(Math.round(times[i]), messageTitleX + i * (histogramWidth + distanceBetweenHistogram), messageHeight - messageRectY - maxHistogramLength - fontHeight);
   }
 };
+
+var messageRectX = 100;
+var messageRectY = 10;
+var shadowOffset = 10;
+var messageWidth = 420;
+var messageHeight = 270;
+var messageTitleX = 130;
+var messageTitleY = 50;
+var fontHeight = 16;
+var histogramLength;
+var maxHistogramLength = 150;
+var histogramWidth = 40;
+var distanceBetweenHistogram = 50;
+var histogramRectY = messageTitleY + fontHeight * 3.125;
+var histogramTransparency = 1;
